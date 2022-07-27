14f68a5ea6c422d51a7245e7f8fef21f9a9738a9
Erskine Gamaliel L. Duenas <91664149+laharl143@users.noreply.github.com>
2022-07-27 11:03:17 +0800

    added some notes

--- notes.js
+++ notes.js
@@ -263,6 +263,52 @@ function getMilk(money) {
   }
 getMilk(7); //output will be a round number due to the function Math.floor
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Output & Return Value
+
+function getMilk(money, costPerBottle) {   
+   console.log("leaveHouse");   
+   console.log("moveRight");
+   console.log("moveRight");
+   console.log("moveUp");
+   console.log("moveUp");
+   console.log("moveUp");
+   console.log("moveUp");
+   console.log("moveRight");
+   console.log("moveRight");
+   
+   console.log("buy " + calcBottles(money, costPerBottle) + " bottles of milk");
+   
+   console.log("moveLeft");
+   console.log("moveLeft");
+   console.log("moveDown");
+   console.log("moveDown");
+   console.log("moveDown");
+   console.log("moveDown");
+   console.log("moveLeft");
+   console.log("moveLeft");
+   console.log("enterHouse");
+
+   return calcChange(money, costPerBottle);
+ }
+
+function calcBottles(startingMoney, costPerBottle) {
+
+   var numberOfBottles = Math.floor(startingMoney / costPerBottle);
+
+   return numberOfBottles;
+}
+
+function calcChange(startingAmount, costPerBottle) {
+   var change = startingAmount % costPerBottle;
+   return change;
+}
+
+console.log("Hello master, here is your " +getMilk(5, 1.5) + " change."); 
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //Challenge: Life in weeks Coding Exercise    https://www.udemy.com/course/the-complete-web-development-bootcamp/learn/quiz/4807840#notes
 
 //My Solution:
